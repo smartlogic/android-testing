efb87bdc53a70a7c1ca98b584b4d7e71a2c765f4
Eric Oestrich <eric@oestrich.org>
2013-07-11 10:43:11 -0400

    Add a button

--- src/test/java/com/example/MainActivityTest.java
+++ src/test/java/com/example/MainActivityTest.java
@@ -1,11 +1,9 @@
 package com.example;
 
-import com.example.MainActivity;
-
+import android.widget.Button;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
@@ -13,15 +11,18 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
-  private MainActivity activity;
+    private MainActivity activity;
+    private Button button;
 
-  @Before
-  public void setUp() {
-    activity = Robolectric.buildActivity(MainActivity.class).create().get();
-  }
+    @Before
+    public void setUp() {
+        activity = Robolectric.buildActivity(MainActivity.class).create().get();
+        button = (Button) activity.findViewById(R.id.button);
+    }
 
-  @Test
-  public void shouldNotBeNull() {
-    assertNotNull(activity);
-  }
+    @Test
+    public void shouldNotBeNull() {
+        assertNotNull("Activity is null", activity);
+        assertNotNull("Button is null", button);
+    }
 }
